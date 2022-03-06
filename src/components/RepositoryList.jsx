bfbc802a5ed88308dedc3db159e35c7553c78052
Elias Fernandes <eliasfernandescout@gmail.com>
2022-03-05 23:56:47 -0300

    feat: create the repositoryList component

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,45 @@
+const repositoryName = 'repositoryName'
+
+
+
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de Repositórios</h1>
+      <ul>
+        <li>
+          <strong>{repositoryName}</strong>
+          <p>Forms in React JS</p>
+          <a href="">
+            Acessar Repositório</a>
+        </li>
+        <li>
+          <strong>{repositoryName}</strong>
+          <p>Forms in React JS</p>
+          <a href="">
+            Acessar Repositório</a>
+        </li>
+        <li>
+          <strong>{repositoryName}</strong>
+          <p>Forms in React JS</p>
+          <a href="">
+            Acessar Repositório</a>
+        </li>
+        <li>
+          <strong>{repositoryName}</strong>
+          <p>Forms in React JS</p>
+          <a href="">
+            Acessar Repositório</a>
+        </li>
+        <li>
+          <strong>{repositoryName}</strong>
+          <p>Forms in React JS</p>
+          <a href="">
+            Acessar Repositório</a>
+        </li>
+      </ul>
+
+
+    </section>
+  )
+}
